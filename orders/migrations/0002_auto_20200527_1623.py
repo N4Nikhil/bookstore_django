1e5c8bd2fc5cedfa5a30bbd9fdce94f01f22d619
N4Nikhil <nikhilkadam131@gmail.com>
2021-04-04 11:50:04 +0530

    Add files via upload

---
+++ orders/migrations/0002_auto_20200527_1623.py
@@ -0,0 +1,17 @@
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('orders', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='order',
+            name='phone',
+            field=models.CharField(max_length=14),
+        ),
+    ]
